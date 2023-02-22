b1d28068c4a8ce21451117f0b53008adce6b82b5
oreste <muhirwa.g.oreste@gmail.com>
2023-02-22 12:03:15 +0100

    Kids table

---
+++ database/migrations/2023_02_22_102502_create_kids_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKidsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kids', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('family_name');
+            $table->string('first_name');
+            $table->date('date_of_birth');
+            $table->boolean('is_handicapped');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kids');
+    }
+}

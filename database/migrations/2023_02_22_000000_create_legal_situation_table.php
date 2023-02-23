1ea04156b88dace79e49a5b91aa798bf9a851987
oreste <muhirwa.g.oreste@gmail.com>
2023-02-23 12:49:05 +0100

    Tables

---
+++ database/migrations/2023_02_22_000000_create_legal_situation_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLegalSituationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('legal_situation', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('label');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('legal_situation');
+    }
+}

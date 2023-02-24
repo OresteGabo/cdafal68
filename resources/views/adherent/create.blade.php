c001ace9d23e9f25cafb191325e2c8f799ffa6d2
oreste <muhirwa.g.oreste@gmail.com>
2023-02-24 16:35:28 +0100

    Files for each class will be added in the folder, for the better codes, and propser use of the layout

---
+++ resources/views/adherent/create.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.layout')
+@section('content')
+    <p>This is create </p>
+@endsection
---
+++ resources/views/adherent/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.layout')
+@section('content')
+<p>This is adherents index</p>
+@endsection

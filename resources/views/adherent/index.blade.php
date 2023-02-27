acbab6058432854d86d4335e9f6e53adbeb7881b
oreste <muhirwa.g.oreste@gmail.com>
2023-02-27 16:03:11 +0100

    Données bruts sont remplacées par une boucle blade, pour les données provient d'un tableau php

--- resources/views/adherent/index.blade.php
+++ resources/views/adherent/index.blade.php
@@ -10,69 +10,27 @@
             <span class="text">Activités recentes</span>
         </div>
         <table class="collapse">
-            <!-- THIS PHP FUNCTION WILL BE REMOVED, AND REAL DATA WILL BE INSERTED -->
-            <?php
-            function randomDate($start_date, $end_date)
-            {
-                $timestamp = rand( strtotime("Jan 01 2020"), strtotime("May 01 2023") );
-                $random_Date = date("d.m.Y", $timestamp );
-                return $random_Date;
-            }
-            ?>
-                <tr >
-                    <th>Intitulé de l'activité <button><i class="uil uil-filter"></i></button></th>
-                    <th>Date heure <button><i class="uil uil-sort"></i></button></th>
-                    <th>Description</th>
-                </tr>
-                <tr>
-                    <td>Ajout d'un adhérent</td>
-                    <td><?php echo randomDate('1-1-2020','1-1-2020') ?></td>
-                    <td>0769097991 <button><i class="uil uil-link small-icon"></i></button></td>
-                </tr>
-                <tr>
-                    <td>Suppression d'un adhérent</td>
-                    <td><?php echo randomDate('1-1-2020','1-1-2020') ?></td>
-                    <td>Manque d'activité <button><i class="uil uil-link small-icon"></i></button></td>
-                </tr>
-                <tr>
-                    <td>Paiement</td>
-                    <td><?php echo randomDate('1-1-2020','1-1-2020') ?></td>
-                    <td>Enregistrement d'un paiement Pour la formation <button><i class="uil uil-link small-icon"></i></button></td>
-                </tr>
-                <tr>
-                    <td>Mise à jour d'un adherent</td>
-                    <td><?php echo randomDate('1-1-2020','1-1-2020') ?></td>
-                    <td>Changement de son adresse <button><i class="uil uil-link small-icon"></i></button></td>
-                </tr>
-                <tr>
-                    <td>Formation</td>
-                    <td><?php echo randomDate('1-1-2020','1-1-2020') ?></td>
-                    <td>Inscription à une formation <button><i class="uil uil-link small-icon"></i></button></td>
-                </tr>
-            <tr>
-                <td>Adhérent</td>
-                <td><?php echo randomDate('1-1-2020','1-1-2020') ?></td>
-                <td>Enregistrement d'un enfant <button><i class="uil uil-link small-icon"></i></button></td>
-            </tr>
-            <tr>
-                <td>Adhérent</td>
-                <td><?php echo randomDate('1-1-2020','1-1-2020') ?></td>
-                <td>MAJ des information d'un enfant <button><i class="uil uil-link small-icon"></i></button></td>
-            </tr>
-            <tr>
-                <td>Adhérent</td>
-                <td><?php echo randomDate('1-1-2020','1-1-2020') ?></td>
-                <td>Enregistrement d'un enfant <button><i class="uil uil-link small-icon"></i></button></td>
-            </tr>
-            <tr>
-                <td>Adhérent</td>
-                <td><?php echo randomDate('1-1-2020','1-1-2020') ?></td>
-                <td>MAJ des information d'un enfant <button><i class="uil uil-link small-icon"></i></button></td>
-            </tr>
 
-
+            <table>
+            <tr>
+                <th>intitule<button><i class="uil uil-filter"></i></button></th>
+                <th>date<button><i class="uil uil-sort"></i></button></th>
+                <th>description</th>
+            </tr>
+            @foreach($activities as $activity)
+                <tr>
+                    <td>{{ $activity['intitule'] }}</td>
+                    <td>{{ $activity['date'] }} </td>
+                    <td>{{ $activity['description'] }}</td>
+                </tr>
+            @endforeach
             </table>
 
-    </div>
+
+
+
+
+
+
 
 @endsection

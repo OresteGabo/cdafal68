291c9d609beff342af765e91f3e59d544da16790
oreste <muhirwa.g.oreste@gmail.com>
2023-02-28 15:57:14 +0100

    Dashboard controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function randomDate()
+    {
+        $timestamp = rand( strtotime("Jan 01 2020"), strtotime("May 01 2023") );
+        $random_Date = date("d.m.Y", $timestamp );
+        return $random_Date;
+    }
+    public function index(){
+        $activities=[
+            ['intitule'=>'Ajout d\'un adhérent','date'=>$this->randomDate(),'description'=>'0769097991'],
+            ['intitule'=>'Suppression d\'un adhérent','date'=>$this->randomDate(),'description'=>'Manque d\'activité'],
+            ['intitule'=>'Paiement','date'=>$this->randomDate(),'description'=>'Enregistrement d\'un paiement Pour la formation'],
+            ['intitule'=>'Mise à jour d\'un adherent','date'=>$this->randomDate(),'description'=>'Changement de son adresse'],
+            ['intitule'=>'Formation','date'=> $this->randomDate(),'description'=>'Inscription à une formation'],
+            ['intitule'=>'Adhérent','date'=>$this->randomDate(),'description'=>'Enregistrement d\'un enfant'],
+            ['intitule'=>'Adhérent','date'=>$this->randomDate(),'description'=>'MAJ des information d\'un enfant'],
+            ['intitule'=>'Adhérent','date'=>$this->randomDate(),'description'=>'Enregistrement d\'un enfant'],
+            ['intitule'=>'Adhérent','date'=>$this->randomDate(),'description'=>'MAJ des information d\'un enfant']
+        ];
+        return view('dashboard.index',['activities'=>$activities]);
+    }
+}

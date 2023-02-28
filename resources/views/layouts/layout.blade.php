4c707727d436d1fc83319235f7ed7458f098b217
oreste <muhirwa.g.oreste@gmail.com>
2023-02-28 15:49:28 +0100

    Layout now can use some bootstrap, esp for tables, so the table design codes in style.css were removed

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,7 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!----======== CSS ======== -->
-    <link rel="stylesheet" href="resources/css/style.css">
+    <!--<link rel="stylesheet" href="resources/css/style.css">-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
     <!-- <link rel="stylesheet" href="styles/sidenav.css">
      <link rel="stylesheet" href="styles/form.css">-->
 
@@ -29,17 +31,19 @@
     <div class="logo-name">
         <div class="logo-image">
             <!--<img src="images/logo.png" alt="">-->
-            <img src="https://lh3.googleusercontent.com/nJAOSkAhOtER2oFvifLRbdZnDEBgE2UHiY38EWaAt7Tv2rU55TuCiEDzrYpganXsMIiTiu5T93XzIwAr4ODJ73wRoZfgdyv2RM9PIX19e_toJACmT96e_xvfJjx_vEEremfsI_18JTD6V46iRs2tjDRSSuo_zLhTh4OCMgu5irlt4z9kHaVDhU-ji-WMuuLuwXmDY_pbRg18dF7vfxeXhfa8kVbCjvzB3wbcAT990QmbhT0msjFA7n-E-1K60KPe2c9nK0Nrf1GauLmJTQkNEU8mciMDfsGcK3ki7pMTaLrJ41kns_gIrQauhXaSNlLTj_Nz616A0_9uu9h38LN2nMVp0MuYWGRjPkuCt_CAZuCb5tOLaZ5nfhDo_3guyEqjyx-vnsSRAjd1FzHHN9gzE1dVgrPBkGlVNNGtT5HxzXX-njT4i-r5jIDTjj2nLhbUzG0YwolVtjNiqSClg2UiUVMnc86aiBwnZ5VdtZdLIZiXVD2UQhCCZntkz2GxRzppdhS9ldDl-WpeP5vcdlF9KEOc11rhHcR5mD7GGwbE2cZ9-DgDwMydRxFqvcncuO-yECWmYW5UvJ5KaoCXWEkp3tSxb7S2D4a6Z1eXvaS7GdVAMlvaZZYzG2PWxnH6FEMCrtV92MtybkxeeeAebQDlYPllNtR2ZFDk6gxgRBBSt5NgHH5RzTFl-jsdI7Q2wdkCgUZsrg9RaUz2NbDf8ygj6cxxrDMAEC8JRJCG8LaHbGsEtTUPsHoxZjf9UNOLnNxHqxlI6LRgZhDJ2JARXQT2pxe5WweIq_fj3aP-nBZz7GtC7EhnVRFyw5CG57u4GpExJ36wHxDyEi4Br-gakZ3KLmOqh6WsF7K8VnlMltclVPqpQmGv6r1AQl7jKA1UllIIZK-ISfdC2kjyA8H1h1B4cMazxmmI2hfOylmAkhPryFKAq1wg1zYry7WOmd2furThL9LIycUJnNqy_8Jw6JWSLtbJjRBW9CF2ZfxSftcpLN-1P1X_pcurEou9LFTgUG4DAg_w6ENaYkrJ8Upr0hdRDrdW=w1187-h968-no?authuser=0" alt="logo cdafal">
+            <img src="/images/logo.png" alt="logo cdafal">
         </div>
 
         <span class="logo_name">CDAFAL 68</span>
     </div>
 
-    <div class="menu-items">
-        <ul class="nav-links">
+    <!--<div class="menu-items">-->
+        <!--<div class="float-left">-->
+    <div class="flex menu-items">
+        <ul class="">
             <li><a href="#">
                     <i class="uil uil-estate"></i>
-                    <span class="link-name">Acceuil</span>
+                    <span class="link-name">Dashboard</span>
                 </a></li>
             <li><a href="#">
 
@@ -86,7 +90,7 @@
     </div>
 </nav>
 
-<section class="dashboard">
+<section class="maincontent">
     <div class="top">
         <i class="uil uil-angle-left-b sidebar-toggle"></i>
 

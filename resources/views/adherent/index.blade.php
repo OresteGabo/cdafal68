a70e341b3876981fe30f9cdb1c36ac6cfad21310
oreste <muhirwa.g.oreste@gmail.com>
2023-02-28 15:56:50 +0100

    Adherent, il a pris l'interface du dashboard

--- resources/views/adherent/index.blade.php
+++ resources/views/adherent/index.blade.php
@@ -6,20 +6,39 @@
 @section('recent_activities')
     <div class="activity">
         <div class="title">
-            <i class="uil uil-clock-three"></i>
-            <span class="text">Activités recentes</span>
-        </div>
-        <table class="collapse">
+            <i class="uil uil-user"></i>
 
-            <table>
-            <tr>
-                <th>intitule<button><i class="uil uil-filter"></i></button></th>
-                <th>date<button><i class="uil uil-sort"></i></button></th>
-                <th>description</th>
-            </tr>
+            <span class="text">Liste des adhérents</span>
+        </div>
+
+
+            <table class="table table-striped table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">intituléss
+                        <!--<label>
+                            <input type="radio" id="last" name="Ppub" value="last"> intitule
+                        </label>-->
+
+                        <!--<label for="selectRange">Intitule</label><select id="selectRange" value="" onchange="radioCheck();">
+                            <option value="">no filtre<i class="uil uil-filter"></i></option>
+                            <option value="20181220-20190120"><input type="radio" id="dewey1" name="drone" value="dewey"></option>
+                            <option value="20180720-20190120"><input type="radio" id="dewey2" name="drone" value="dewey"></option>
+                            <option value="20180120-20190120"><input type="radio" id="dewey3" name="drone" value="dewey"></option>
+                        </select>-->
+
+                        <!--<button></button>-->
+                    </th>
+                    <th scope="col">date<button><i class="uil uil-sort"></i></button></th>
+                    <th scope="col">description</th>
+                </tr>
+            </thead>
             @foreach($activities as $activity)
                 <tr>
-                    <td>{{ $activity['intitule'] }}</td>
+                    <th scope="row">{{ $loop->index +1 }}</th>
+                    <td>{{ $activity['intitule'] }}
+                    </td>
                     <td>{{ $activity['date'] }} </td>
                     <td>{{ $activity['description'] }}</td>
                 </tr>
@@ -27,10 +46,14 @@
             </table>
 
 
-
-
-
-
-
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+        </nav>
 
 @endsection

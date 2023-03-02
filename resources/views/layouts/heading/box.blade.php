28f99d25977c64a78e72c0018b340b21c96c21ac
oreste <muhirwa.g.oreste@gmail.com>
2023-03-02 02:09:20 +0100

    box div turned into a link

--- resources/views/layouts/heading/box.blade.php
+++ resources/views/layouts/heading/box.blade.php
@@ -1,5 +1,7 @@
-<div class="box box{{$box_number}}">
+<a href="{{ $url ??'#' }}" class="box box{{$box_number}}">
     <i class="{{$icon}}"></i>
     <span class="text">{{$label}}</span>
     <span class="number">{{$data}}</span>
-</div>
+</a>
+
+

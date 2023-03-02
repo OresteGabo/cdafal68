ff2b0000f9c27d6856fcebf1ae252dcae27fc7ce
oreste <muhirwa.g.oreste@gmail.com>
2023-03-02 10:27:16 +0100

    login and sign up views

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,8 +6,12 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+
     public function index(){
-        return view('login');
+        return view('auth.index');
+    }
+    public function signup(){
+        return view('auth.signup');
     }
 
 }

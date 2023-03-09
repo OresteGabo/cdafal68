243127dd3d07a7a6f77bf6dee695fff3bac05fcd
oreste <muhirwa.g.oreste@gmail.com>
2023-03-09 15:36:49 +0100

    city with postal code foreign key

---
+++ database/migrations/2023_02_22_000001_create_cities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('postal_code_id');
+            $table->foreign('postal_code_id')->references('id')->on('postal_code')->onDelete('cascade');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}

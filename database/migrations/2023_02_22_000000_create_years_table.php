f359a7396f7324b10fab91bd474f85c913140bad
oreste <muhirwa.g.oreste@gmail.com>
2023-03-09 15:28:23 +0100

    timestamps deleted

--- database/migrations/2023_02_22_000000_create_years_table.php
+++ database/migrations/2023_02_22_000000_create_years_table.php
@@ -15,7 +15,6 @@ class CreateYearsTable extends Migration
     {
         Schema::create('years', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->unsignedSmallInteger('label');
         });
     }

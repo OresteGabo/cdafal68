9b83d3e18c985fb9615fbdd9ec860168778f690d
oreste <muhirwa.g.oreste@gmail.com>
2023-03-16 10:29:21 +0100

    Arrays data were changed into database data, the modification to adapt the change, access from an array and access from an object

--- resources/views/layouts/selects/options.blade.php
+++ resources/views/layouts/selects/options.blade.php
@@ -1,3 +1,5 @@
+
 @foreach ($data_array as $data_item)
-    <option value="{{  $data_item['value']?? $data_item['label']}}">{{ $data_item['label'] }}</option>
+    <option value="{{  $data_item->id}}">{{ $data_item->label }}</option>
 @endforeach
+

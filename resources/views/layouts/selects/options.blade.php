2bd7e897843969d7b30016d0810a09b73e75612c
oreste <muhirwa.g.oreste@gmail.com>
2023-03-20 14:33:05 +0100

    Adjustement to the cities change

--- resources/views/layouts/selects/options.blade.php
+++ resources/views/layouts/selects/options.blade.php
@@ -1,5 +1,11 @@
 
 @foreach ($data_array as $data_item)
-    <option value="{{  $data_item->id}}">{{ $data_item->label }}</option>
+    <option value="{{  $data_item->id}}">
+        @if(isset($data_item->cp))
+            {{ $data_item->cp }} - {{ $data_item->label }}
+        @else
+            {{ $data_item->label }}
+        @endif
+    </option>
 @endforeach
 

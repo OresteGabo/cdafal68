0a0b108acfc8d7dc44ab11266277e4cfce897626
oreste <muhirwa.g.oreste@gmail.com>
2023-03-23 14:40:52 +0100

    Adding of a tooltip text

--- resources/views/layouts/heading/box.blade.php
+++ resources/views/layouts/heading/box.blade.php
@@ -1,7 +1,8 @@
 <a href="{{ $url ??'#' }}" class="box box{{$box_number}}">
     <i class="{{$icon}}"></i>
-    <span class="text">{{$label}}</span>
-    <span class="number">{{$data}}</span>
+    <span class="text">{{$label??''}}</span>
+    <span class="number">{{$data ?? ''}}</span>
+    <div class="tooltiptext">{{$tooltiptext ?? ''}}</div>
 </a>
 
 

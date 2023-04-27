617fe04d0fcef44b67ded58e60a14b0b3a2534a6
oreste <muhirwa.g.oreste@gmail.com>
2023-04-27 11:43:45 +0200

    overriding  value for precising which database table will be used, et la desactivation du timestamps pour pas que l'ajout de valeur ne nous demande pas la valeur de updated_at

--- app/Models/StudyLevel.php
+++ app/Models/StudyLevel.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class StudyLevel extends Model
 {
     use HasFactory;
+    protected $table='study_levels';
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 }

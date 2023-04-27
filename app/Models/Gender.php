1f641ebb052892629d3190ead82a1d3dcd6a9456
oreste <muhirwa.g.oreste@gmail.com>
2023-04-27 11:19:11 +0200

    overriding  value for precising which database table will be used, et la desactivation du timestamps pour pas que l'ajout de valeur ne nous demande pas la valeur de updated_at

--- app/Models/Gender.php
+++ app/Models/Gender.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Gender extends Model
 {
     use HasFactory;
+    protected $table='genders';
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 }

8fbce839010e5441cdb6a8648cefbeafb0dea193
oreste <muhirwa.g.oreste@gmail.com>
2023-05-09 09:40:57 +0200

    The show and the destroy of an individual Income type updated

--- app/Http/Controllers/IncomeTypeController.php
+++ app/Http/Controllers/IncomeTypeController.php
@@ -51,6 +51,7 @@ class IncomeTypeController extends Controller
     public function show($id)
     {
         //
+        return view('settings.incometype.show',['incometype'=>IncomeType::findOrFail($id)]);
     }
 
     /**
@@ -85,5 +86,8 @@ class IncomeTypeController extends Controller
     public function destroy($id)
     {
         //
+        $data=IncomeType::findOrFail($id);
+        $data->delete();
+        return view('settings.incometype.index');
     }
 }
